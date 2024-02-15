b181b79af9ed41bd1f350ab8dc9023b6097b3419
Sahil Mahadwar <accounts@mahadwar.com>
2024-02-15 11:30:49 +0530

    first commit

---
+++ keystatic.config.ts
@@ -0,0 +1,33 @@
+// keystatic.config.ts
+import { collection, config, fields } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "github",
+    repo: {
+      name: "astro-blog",
+      owner: "SahilMahadwar",
+    },
+  },
+  collections: {
+    posts: collection({
+      label: "blog",
+      slugField: "title",
+      path: "src/content/blog/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        description: fields.text({ label: "Description" }),
+        pubDate: fields.date({ label: "PubDate" }),
+
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
+  },
+});
